df139e89d1cd01b18439b02864cf10013a66f864
Aakriti Jain <jainaakriti@johndeere.com>
2019-09-22 13:25:10 +0530

    removed unused controller

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import * as Hapi from '@hapi/hapi';
 import {loginRoute} from './controllers/login';
-import {registerRoute} from './controllers/register';
 
 var server = new Hapi.server({
     host: 'localhost',
@@ -10,7 +9,7 @@ var server = new Hapi.server({
 const init = async () => {
     try {
         console.log("hello");
-        server.route([loginRoute, registerRoute]);
+        server.route([loginRoute]);
         await server.start();
         console.log("server started at: ", server.settings.port);
     } catch (err) {
