98997bfe6349e5f69dcf7667d187c5b1f9bbdbdc
Aakriti Jain <jainaakriti@johndeere.com>
2019-09-29 02:20:15 +0530

    refactor error handling and fix update user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,26 +1,90 @@
-import {getUsersById, updateUsersById} from '../services/users';
+import {createUser, getUserById, getUsersByPhoneNumberAndUserType, updateUserById} from '../services/users';
+import {errorMessages} from '../util/constants';
+import {isAValidPhoneNumber, isAValidUserType, validateId} from '../util/validator';
 
-export const usersRoute = [{
-    path: '/users/{id}',
-    method: 'GET',
-    options: {
-        handler: async (request) => {
-            const id = request.params.id;
-            return await getUsersById(id);
+export const usersRoute = [
+    {
+        path: '/users/{id}',
+        method: 'GET',
+        options: {
+            handler: async (request) => {
+                const id = request.params.id;
+                return await getUserById(id)
+                    .catch((err) => err);
+            }
+        }
+    },
+    {
+        path: '/users/{id}',
+        method: 'PUT',
+        options: {
+            handler: async (request) => {
+                const id = request.params.id;
+                const {
+                    id: idFromPayload,
+                    firstName,
+                    secondName,
+                    email
+                } = request.payload;
+                validateId(id, idFromPayload);
+                return await updateUserById(firstName, secondName, email, id)
+                    .then((result) => {
+                        if (result.affectedRows === 0)
+                            return errorMessages.ENTITY_DOESNT_EXIST;
+                        else
+                            return {
+                                id,
+                                firstName,
+                                secondName,
+                                email
+                            }
+                    })
+                    .catch((err) => err);
+            }
+        }
+    },
+    {
+        path: '/users',
+        method: 'POST',
+        options: {
+            handler: async (request) => {
+                const {
+                    phoneNumber,
+                    userType
+                } = request.payload;
+                let errors = [];
+                userType.toLowerCase();
+                const validPhoneNumber = isAValidPhoneNumber(phoneNumber);
+                const validUserType = isAValidUserType(userType);
+
+
+                if (validPhoneNumber && validUserType) {
+                    const user = await getUsersByPhoneNumberAndUserType(phoneNumber, userType)
+                        .catch((err) => err);
+                    if (user.length) {
+                        return errorMessages.ExistsAlready;
+                    } else {
+                        const userId = await createUser(phoneNumber, userType)
+                            .catch((err) => err);
+                        if (userId) {
+                            return await getUserById(userId)
+                                .catch((err) => err);
+                        }
+                    }
+                } else {
+                    if (!validPhoneNumber) {
+                        errors.push({
+                            errorMessage: errorMessages.InvalidPhoneNumber
+                        });
+                    }
+                    if (!validUserType) {
+                        errors.push({
+                            errorMessage: errorMessages.InvalidUserType
+                        });
+                    }
+                }
+                return errors;
+            }
         }
     }
-}, {
-    path: '/users/{id}',
-    method: 'PUT',
-    options: {
-        handler: async (request) => {
-            const id = request.params.id;
-            const {
-                firstName,
-                secondName,
-                email
-            } = request.payload;
-            return await updateUsersById(firstName, secondName, email, id);
-        }
-    }
-}];
+];
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,16 +1,18 @@
 export const phoneRegex = /^[7-9]\d{9}$/;
-export const userTypes = ['ADMIN','SUPER_ADMIN','USER','CLIENT'];
+export const userTypes = ['admin', 'superAdmin', 'user', 'client'];
 export const errorMessages = {
     LoginFailed: 'LOGIN_FAILED',
     RegisterFailed: 'REGISTER_FAILED',
     InvalidPhoneNumber: 'INVALID_PHONE_NUMBER',
-    InvalidUserType: 'INVALID_USER_TYPE'
+    InvalidUserType: 'INVALID_USER_TYPE',
+    ExistsAlready: 'EXISTS_ALREADY'
 };
 
 export const ERROR_MAPPINGS = {
     ER_DUP_ENTRY: 'DUPLICATE_ENTRY',
     ER_ROW_IS_REFERENCED_2: 'ROW_REFERENCED_CANNOT_BE_DELETED',
-    ENTITY_DOESNT_EXIST: 'DOESNT_EXIST',
+    ENTITY_DOESNT_EXIST: 'ENTITY_DOESNT_EXIST',
     DEFAULT: 'ERROR_OCCURRED',
-    INVALID_PAYLOAD: 'INVALID_PAYLOAD'
+    INVALID_PAYLOAD: 'INVALID_PAYLOAD',
+    ER_BAD_NULL_ERROR: 'CANNOT_NULL'
 };
